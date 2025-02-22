ff197743c4359e451143f6654ed7a3300017a194
2102582AlexButuc <a.butuc@rgu.ac.uk>
2025-02-22 17:57:49 +0000

    add HomePlanet and placeholder sprite

--- Assets/PlayerCharacter/PlayerMovement.cs
+++ Assets/PlayerCharacter/PlayerMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
     public Rigidbody2D body;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,6 +16,5 @@ public class PlayerMovement : MonoBehaviour
     {
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
-        body.linearVelocity = new Vector2(xInput, yInput);
-    }
+        body.linearVelocity = new Vector2(xInput, yInput).normalized;    }
 }
